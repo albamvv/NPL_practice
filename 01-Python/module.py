f8f6646404704b2dc5fccacfbd41afd7ebcbb6b8
Alba Maitia Vadillo Valderrama <alba.vadillo3@gmail.com>
2025-03-18 15:48:37 +0100

    basics

---
+++ 01-Python/module.py
@@ -0,0 +1,5 @@
+def sqaure(x):
+    return x**2
+
+def greet(name):
+    return f"Hello {name}!!!!!!!!!"
---
+++ 01-Python/scripts/module2.py
@@ -0,0 +1,11 @@
+def sqaure(x):
+    return x**2
+
+def greet(name):
+    return f"Hello {name}#######"
+
+
+if __name__ == '__main__':
+    print("This module2 script is being run directly")
+else:
+    print("This module2 script is being imported")
